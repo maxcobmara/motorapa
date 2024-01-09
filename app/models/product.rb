3c49c2c80743038d3513a1fdc4dc2bafdd1e1825
Yaacob Noor <maxcobmara@gmail.com>
2024-01-09 12:13:48 +0800

    fixes for product saving

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   friendly_id :slug_candidates, use: :slugged
   belongs_to :manufacturer
   
-  validates_presence_of :model,:site_category, :displacement
+  validates_presence_of :model,:category, :displacement
   
   def slug_candidates
     [
