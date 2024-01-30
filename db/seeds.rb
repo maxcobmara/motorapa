f0bcc0e8b305692d15dc3d9f94f0b1f99485c0bc
Yaacob Noor <maxcobmara@gmail.com>
2024-01-30 17:32:26 +0800

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,80 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-puts "setting up manufacturing facilities"
-
-manufacturers = ["Adiva", "AJS", "Aprilia", "Aveta", "Benelli", "Bimota", "BKZ", "Blueshark", "BMW", "Brixton", "Buell", "Can-Am", "CFMoto", "Comel", "CMC", "Daelim", "Daiichi", "Demak", "Ducati", "Fantic", "Gilera", "GPX", "Harley-Davidson", "Honda", "Husaberg", "Husqvarna", "Hyosung", "Indian", "Italjet", "Jawa", "Kawasaki", "Kayo", "Keeway", "KTM", "KTNS", "Kymco", "Lambretta", "Laverda", "LIMA", "MLE", "Modenas", "Momos", "Moto Guzzi", "Moto Morini", "MV Agusta", "Mz", "Naza", "Nimota", "Nitro", "Norton", "Ottimo", "Petronas", "Piaggio", "Royal Alloy", "Royal Enfield", "QJ Motor", "Scomadi", "Sherco", "Skyteam", "SM Sport", "Steyr Daimler", "Suzuki", "SYM", "Treeletrik", "Triumph", "TVS", "Vespa", "Victory", "WMOTO", "X-MOTO", "Yamaha", "Yadea", "Zero Engineering", "Zesparii", "Zontes"]
-
-manufacturers.each do | m |
-  Manufacturer.find_or_create_by!(name: m)
-end
+Manufacturer.create!([
+  {slug: "adiva", name: "Adiva", website: nil, country: nil, data: nil},
+  {slug: "ajs", name: "AJS", website: nil, country: nil, data: nil},
+  {slug: "aprilia", name: "Aprilia", website: nil, country: nil, data: nil},
+  {slug: "aveta", name: "Aveta", website: nil, country: nil, data: nil},
+  {slug: "benelli", name: "Benelli", website: nil, country: nil, data: nil},
+  {slug: "bimota", name: "Bimota", website: nil, country: nil, data: nil},
+  {slug: "bkz", name: "BKZ", website: nil, country: nil, data: nil},
+  {slug: "blueshark", name: "Blueshark", website: nil, country: nil, data: nil},
+  {slug: "bmw", name: "BMW", website: nil, country: nil, data: nil},
+  {slug: "brixton", name: "Brixton", website: nil, country: nil, data: nil},
+  {slug: "buell", name: "Buell", website: nil, country: nil, data: nil},
+  {slug: "can-am", name: "Can-Am", website: nil, country: nil, data: nil},
+  {slug: "cfmoto", name: "CFMoto", website: nil, country: nil, data: nil},
+  {slug: "comel", name: "Comel", website: nil, country: nil, data: nil},
+  {slug: "cmc", name: "CMC", website: nil, country: nil, data: nil},
+  {slug: "daelim", name: "Daelim", website: nil, country: nil, data: nil},
+  {slug: "daiichi", name: "Daiichi", website: nil, country: nil, data: nil},
+  {slug: "demak", name: "Demak", website: nil, country: nil, data: nil},
+  {slug: "ducati", name: "Ducati", website: nil, country: nil, data: nil},
+  {slug: "fantic", name: "Fantic", website: nil, country: nil, data: nil},
+  {slug: "gilera", name: "Gilera", website: nil, country: nil, data: nil},
+  {slug: "gpx", name: "GPX", website: nil, country: nil, data: nil},
+  {slug: "harley-davidson", name: "Harley-Davidson", website: nil, country: nil, data: nil},
+  {slug: "honda", name: "Honda", website: nil, country: nil, data: nil},
+  {slug: "husaberg", name: "Husaberg", website: nil, country: nil, data: nil},
+  {slug: "husqvarna", name: "Husqvarna", website: nil, country: nil, data: nil},
+  {slug: "hyosung", name: "Hyosung", website: nil, country: nil, data: nil},
+  {slug: "indian", name: "Indian", website: nil, country: nil, data: nil},
+  {slug: "italjet", name: "Italjet", website: nil, country: nil, data: nil},
+  {slug: "jawa", name: "Jawa", website: nil, country: nil, data: nil},
+  {slug: "kawasaki", name: "Kawasaki", website: nil, country: nil, data: nil},
+  {slug: "kayo", name: "Kayo", website: nil, country: nil, data: nil},
+  {slug: "keeway", name: "Keeway", website: nil, country: nil, data: nil},
+  {slug: "ktm", name: "KTM", website: nil, country: nil, data: nil},
+  {slug: "ktns", name: "KTNS", website: nil, country: nil, data: nil},
+  {slug: "kymco", name: "Kymco", website: nil, country: nil, data: nil},
+  {slug: "lambretta", name: "Lambretta", website: nil, country: nil, data: nil},
+  {slug: "laverda", name: "Laverda", website: nil, country: nil, data: nil},
+  {slug: "lima", name: "LIMA", website: nil, country: nil, data: nil},
+  {slug: "mle", name: "MLE", website: nil, country: nil, data: nil},
+  {slug: "modenas", name: "Modenas", website: nil, country: nil, data: nil},
+  {slug: "momos", name: "Momos", website: nil, country: nil, data: nil},
+  {slug: "moto-guzzi", name: "Moto Guzzi", website: nil, country: nil, data: nil},
+  {slug: "moto-morini", name: "Moto Morini", website: nil, country: nil, data: nil},
+  {slug: "mv-agusta", name: "MV Agusta", website: nil, country: nil, data: nil},
+  {slug: "mz", name: "Mz", website: nil, country: nil, data: nil},
+  {slug: "naza", name: "Naza", website: nil, country: nil, data: nil},
+  {slug: "nimota", name: "Nimota", website: nil, country: nil, data: nil},
+  {slug: "nitro", name: "Nitro", website: nil, country: nil, data: nil},
+  {slug: "norton", name: "Norton", website: nil, country: nil, data: nil},
+  {slug: "ottimo", name: "Ottimo", website: nil, country: nil, data: nil},
+  {slug: "petronas", name: "Petronas", website: nil, country: nil, data: nil},
+  {slug: "piaggio", name: "Piaggio", website: nil, country: nil, data: nil},
+  {slug: "royal-alloy", name: "Royal Alloy", website: nil, country: nil, data: nil},
+  {slug: "royal-enfield", name: "Royal Enfield", website: nil, country: nil, data: nil},
+  {slug: "qj-motor", name: "QJ Motor", website: nil, country: nil, data: nil},
+  {slug: "scomadi", name: "Scomadi", website: nil, country: nil, data: nil},
+  {slug: "sherco", name: "Sherco", website: nil, country: nil, data: nil},
+  {slug: "skyteam", name: "Skyteam", website: nil, country: nil, data: nil},
+  {slug: "sm-sport", name: "SM Sport", website: nil, country: nil, data: nil},
+  {slug: "steyr-daimler", name: "Steyr Daimler", website: nil, country: nil, data: nil},
+  {slug: "suzuki", name: "Suzuki", website: nil, country: nil, data: nil},
+  {slug: "sym", name: "SYM", website: nil, country: nil, data: nil},
+  {slug: "treeletrik", name: "Treeletrik", website: nil, country: nil, data: nil},
+  {slug: "triumph", name: "Triumph", website: nil, country: nil, data: nil},
+  {slug: "tvs", name: "TVS", website: nil, country: nil, data: nil},
+  {slug: "vespa", name: "Vespa", website: nil, country: nil, data: nil},
+  {slug: "victory", name: "Victory", website: nil, country: nil, data: nil},
+  {slug: "wmoto", name: "WMOTO", website: nil, country: nil, data: nil},
+  {slug: "x-moto", name: "X-MOTO", website: nil, country: nil, data: nil},
+  {slug: "yamaha", name: "Yamaha", website: nil, country: nil, data: nil},
+  {slug: "yadea", name: "Yadea", website: nil, country: nil, data: nil},
+  {slug: "zero-engineering", name: "Zero Engineering", website: nil, country: nil, data: nil},
+  {slug: "zesparii", name: "Zesparii", website: nil, country: nil, data: nil},
+  {slug: "zontes", name: "Zontes", website: nil, country: nil, data: nil}
+])
+Product.create!([
+  {slug: "yamaha-y15zr", manufacturer_id: "71", category: "Underbone", manufacturer_category: "", model: "Y15ZR", model_code: "B17E", rrp: "9498.0", assembled_in: nil, seat_height_mm: 845, wheelbase_mm: 1290, length_mm: 1960, height_mm: 1080, width_mm: 670, ground_clearance_mm: 135, engine_description: "Liquid-cooled, 4-stroke SOHC", displacement: 150, max_power_ps: "14.9", max_power_rpm: 8500, max_torque_nm: "13.8", max_torque_rpm: 7000, cylinders: 1, cyl_bore: "57.0", cyl_stroke: "58.7", cyl_valves: 4, oil_capacity_l: "1.15", fuel: "Petrol", fuel_capacity_l: "4.2", consumption_lkm: nil, range_km: nil, drive_category: 0, transmission: "Manual", clutch: "", gears: 5, tyre_front: "70/90 R17", tyre_rear: "120/70 R17", tube: false, tyre_category: "Radial", wheel_front_in: 17, wheel_rear_in: 17, suspension_front: "", suspension_rear: "", kerb_weight_kg: 115, brake_front_desc: "", brake_rear_desc: "", battery_size: "", additional_features: "", data: ""}
+])
