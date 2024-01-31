f250c9d5db51b71eacfe35640e2e70dca123e1a9
Yaacob Noor <maxcobmara@gmail.com>
2024-01-31 13:07:41 +0800

    complete manufacturer seed

--- app/helpers/selector.rb
+++ app/helpers/selector.rb
@@ -1,4 +1,4 @@
 module Selector
-  MCOUNTRY = %w(MY AT CN GB DE IT JP US  )
+  MCOUNTRY = %w(MY AT CN GB DE IT JP US TW )
   BIKECAT  = %w(Naked Cruiser Sport Touring Sport\ Touring Off\ Road Adventure Scooter Underbone Moped Pit Trike  )
 end
