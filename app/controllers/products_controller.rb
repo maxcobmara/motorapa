07412b7c7ac871bbc309aae2cdf98cc74c84d975
Yaacob Noor <maxcobmara@gmail.com>
2024-03-13 14:44:17 +0800

    landing mvp kinda done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,12 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    if params[:category]
+      @products = Product.where(category: params[:category])
+    else
+      @products = Product.all
+    end
+    
   end
 
   # GET /products/1 or /products/1.json
