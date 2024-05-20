d0dd1de53d76d64007116ecb6e01de0cf3cd07bb
Yaacob Noor <maxcobmara@gmail.com>
2024-05-20 16:08:32 +0800

    coming soon page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   
   
   def home
-    
+  end
+  
+  def coming_soon
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "pages#home"
+  #root to: "pages#home"
+  root to: "pages#coming_soon"
+  
 end
